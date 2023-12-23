29495c8c38a690dbb3139268f38a53b4db1997a7
Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>
2023-12-22 22:26:09 -0300

    Aluno e Boleto Exception
    Signed-off-by: Gabriel Donizete dos Santos Lima <gabrieldonizete0@gmail.com>

---
+++ src/main/java/br/com/faculdade/boleto/exception/AlunoException.java
@@ -0,0 +1,8 @@
+package br.com.faculdade.boleto.exception;
+
+public class AlunoException extends RuntimeException{
+
+	public AlunoException (String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/faculdade/boleto/exception/BoletoException.java
@@ -0,0 +1,8 @@
+package br.com.faculdade.boleto.exception;
+
+public class BoletoException extends RuntimeException{
+
+	 public BoletoException(String message) {
+	        super(message);
+	   }
+}
